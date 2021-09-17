a310f31dcd461349844ff9c81f6e2f753693dded
Roger Schaer <roger.schaer@hevs.ch>
2021-09-17 15:01:48 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM node:14
+MAINTAINER Roger Schaer <roger.schaer@hevs.ch>
+
+# Create app directory
+WORKDIR /usr/src/app
+
+# Install app dependencies
+COPY package.json ./
+COPY yarn.lock ./
+RUN yarn
+
+# Bundle app source
+COPY . .
+
+# Expose port
+EXPOSE 3000
+
+# Start the app
+CMD [ "npm", "start" ]
---
+++ routes/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+
+const admin = require("firebase-admin");
+
+const serviceAccount = require("../serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  //databaseURL: "https://pfyn-finges-nature-park-default-rtdb.europe-west1.firebasedatabase.app"
+});
+
+/* GET home page. */
+router.get("/", async function (req, res, next) {
+  let { users } = await admin.auth().listUsers();
+  console.log("users", users);
+
+  res.render("index", {
+    title: "Pfyn-Finges Permissions Manager",
+    users: users,
+  });
+});
+
+/* MAKE user an admin */
+router.patch("/user/:uid/:mode", async function (req, res, next) {
+  let uid = req.params.uid;
+  let mode = req.params.mode;
+
+  console.log(`Make user ${uid} a ${mode}...`);
+
+  await admin.auth().setCustomUserClaims(uid, { admin: mode === "admin" });
+
+  res.send({ message: "OK" });
+});
+
+module.exports = router;
